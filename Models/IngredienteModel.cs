1877de3023c13950d1aa876169a943cbdafe46dc
Eduardo P.P. Ferreira <ppfeduardo@gmail.com>
2025-04-18 17:28:12 -0300

    Criando CRUD de ingredientes

--- Models/IngredienteModel.cs
+++ Models/IngredienteModel.cs
@@ -3,10 +3,20 @@ namespace api_receitas.Models;
 public class IngredienteModel(string nome, string unidade)
 {
     public Guid Id { get; init; } = Guid.NewGuid();
-    public string Nome { get; private set; } = nome;
-    public string Unidade { get; private set; } = unidade;
+    public string? Nome { get; private set; } = nome;
+    public string? Unidade { get; private set; } = unidade;
     public bool IsAtivo { get; set; } = true; // Para soft delete na base de dados
     // public string? Descricao { get; set; }
     // public string? Tipo { get; set; }
     // public double? Quantidade { get; set; }
+
+    public void Atualizar(string nome, string unidade)
+    {
+        Nome = nome;
+        Unidade = unidade;
+    }
+    public void Desativar()
+    {
+        IsAtivo = false;
+    }
 }
