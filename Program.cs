b1cd94f7f5dbbcfbab6c5ddea9d0341868505d74
Eduardo P.P. Ferreira <ppfeduardo@gmail.com>
2025-04-18 16:50:02 -0300

    Criando Ingrediente na base de dados

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
+using api_receitas.Data;
+using api_receitas.Routes;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IngredienteContext>();
 
 var app = builder.Build();
 
@@ -14,6 +18,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.IngredienteRoutes();
 
+app.UseHttpsRedirection();
 app.Run();
